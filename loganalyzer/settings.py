bac84b6560e09bfe61f0b45b634f6b7145290812
Ørjan Jacobsen <47573432+orjanj@users.noreply.github.com>
2021-04-15 09:31:39 +0200

    Update settings.py

--- loganalyzer/settings.py
+++ loganalyzer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&t#u23kq5smmw5m%7w2($%e8zvsxwf))t(i*@i@i-kvei3rb&0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
